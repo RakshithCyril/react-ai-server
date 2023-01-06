8d551cde387057402724390ca9530ff504dc0dbd
Rithin Cyril <rithincyr.lms@gmail.com>
2023-01-06 17:24:25 +0530

    validation

--- server.js
+++ server.js
@@ -33,8 +33,8 @@ const configuration = new Configuration({
 })
 
 const cors = require('cors')
+const { validate } = require('./models/users')
 const openai = new OpenAIApi(configuration)
-
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
@@ -45,20 +45,20 @@ const corsOptions = {
   optionsSuccessStatus: 200
 }
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
   res.send('connected')
 })
 
-app.post('/login',async (req, res) => {
-  const {email, password} = req.body
-  const valEmail = await Users.findOne({email:email})
-  if(!valEmail){
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body
+  const valEmail = await Users.findOne({ email: email })
+  if (!valEmail) {
     res.send(true)
-  }else{
-    const valPass = await bcrypt.compare(password,valEmail.pass)
-    if(valPass){
+  } else {
+    const valPass = await bcrypt.compare(password, valEmail.pass)
+    if (valPass) {
       res.send(false)
-    }else{
+    } else {
       res.send(true)
     }
   }
@@ -67,23 +67,30 @@ app.post('/login',async (req, res) => {
 app.post('/signup', async (req, res) => {
   const { fname, lname, email, password } = req.body
   const bodyObj = req.body
+
+  // const hash = await bcrypt.hash(password, 12)
+  const validate = await Users.findOne({ email: email })
+  if(!validate){
+    res.send('done')
+     const user = new Users({
+      fname: fname,
+      lname: lname,
+      email: email,
+      pass: hash
+    })
+    await user
+      .save()
+      .then(dat => {
+        console.log(dat)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }else{
+    res.send(true)
+  }
+   
   
-  const hash = await bcrypt.hash(password,12)
-  const user = new Users({
-    fname: fname,
-    lname: lname,
-    email: email,
-    pass: hash
-  })
-  await user
-    .save()
-    .then(dat => {
-      console.log(dat)
-    })
-    .catch(err => {
-      console.log(err)
-    })
-  res.send('done!!')
 })
 
 app.post('/api', async (req, res) => {
