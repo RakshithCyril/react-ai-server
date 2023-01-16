1eb7854c185d936cfd9faa307bfca4298e7bb68f
Rithin Cyril <rithincyr.lms@gmail.com>
2023-01-17 01:26:50 +0530

    cors update cyc

--- server.js
+++ server.js
@@ -35,15 +35,15 @@ const configuration = new Configuration({
 const cors = require('cors')
 const { validate } = require('./models/users')
 const openai = new OpenAIApi(configuration)
-const corsOptions = {
-  credentials: true,
-  origin: "http://localhost:3000",
-  optionsSuccessStatus: 200
-}
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-app.use(cors(corsOptions))
-
+app.use(cors())
+const corsOptions = {
+  credentials: true,
+  origin: ['https://natalia-uxy3.onrender.com/', 'http://192.168.1.2:3000'],
+  optionsSuccessStatus: 200
+}
 
 app.get('/', async(req, res) => {
   res.send('connected')
